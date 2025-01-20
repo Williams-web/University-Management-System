4ee1e1ab490b36e89f8945540ce7c1b6a200afa9
Yaro Williams <yarowilliams402@gmail.com>
2025-01-20 22:41:55 +0000

    Update UniversityDriverMenu.java
    I have added a few input validations

--- UniversityDriverMenu.java
+++ UniversityDriverMenu.java
@@ -10,25 +10,54 @@ public class UniversityDriverMenu {
 
         System.out.println("Enter Student Name: ");
         student.studentName = input.nextLine();
+        if (student.studentName.isEmpty()){
+            System.out.println("Student name cannot be empty");
+            return;
+        }
 
         System.out.println("Enter Registration Number: ");
         student.regdNo = input.nextLine();
+        if (student.regdNo.isEmpty()){
+            System.out.println("Registration number cannot be empty");
+            return;
+        }
 
         System.out.println("Enter Elective Subject: ");
         student.electiveSubject = input.nextLine();
+        if (student.electiveSubject.isEmpty()){
+            System.out.println("Elective subject cannot be empty");
+            return;
+        }
 
         System.out.println("Enter Average Marks: ");
         student.avgMarks = input.nextDouble();
+        if (student.avgMarks <0){
+            System.out.println("Average mark cannot be less than 0");
+            return;
+        }
         input.nextLine();
+
 
         System.out.println("Enter Hostel Name: ");
         student.hostelName = input.nextLine();
+        if (student.hostelName.isEmpty()){
+            System.out.println("Hostel name cannot be empty");
+            return;
+        }
 
         System.out.println("Enter Hostel Location: ");
         student.hostelLocation = input.nextLine();
+        if (student.hostelLocation.isEmpty()){
+            System.out.println("Hostel location cannot be empty");
+            return;
+        }
 
         System.out.println("Enter Number of Rooms: ");
         student.numberOfRooms = input.nextInt();
+        if (student.numberOfRooms < 1 ){
+            System.out.println("Number of rooms cannot be less than 1");
+            return;
+        }
         input.nextLine();
 
         studentList.add(student);
@@ -40,17 +69,33 @@ public class UniversityDriverMenu {
 
         System.out.println("Enter your Registration number ");
        String regdNo = input.nextLine();
+       if (regdNo.isEmpty()){
+           System.out.println("Registration number cannot be empty");
+           return;
+       }
 
        for(Student student : studentList){
            if(student.regdNo.equals(regdNo)){
                System.out.println("Enter new Hostel Name: ");
                student.hostelName = input.nextLine();
+               if (student.hostelName.isEmpty()){
+                   System.out.println("Hostel name cannot be empty");
+                   return;
+               }
 
                System.out.println("Enter new Hostel Location: ");
                student.hostelLocation = input.nextLine();
+                if (student.hostelLocation.isEmpty()){
+                     System.out.println("Hostel location cannot be empty");
+                     return;
+                }
 
                System.out.println("Enter new Number of Rooms: ");
                student.numberOfRooms = input.nextInt();
+               if (student.numberOfRooms < 1){
+                   System.out.println("Number of rooms cannot be less than 1");
+                   return;
+               }
 
                System.out.println("Student migrated successfully!");
            }else {
